70db5d89b2f8c4725877df811f9eab674f437aff
Nataliia Tenkova <natalia__korotkova@mail.ru>
2021-07-13 22:55:55 +0300

    Update part_02 task_03

--- module_03/part_02/task_03.py
+++ module_03/part_02/task_03.py
@@ -1,13 +1,10 @@
 d = {'name1': 'id1', 'name2': 'id1', 'name3': 'id3'}
 a = {}
 for item in d:
-	list_value = []
-	list_value.append(item)
+	list_value = [item]
 	if d[item] in a:
 		list_value.extend(a[d[item]])
-		a[d[item]] = list_value
-	else:
-		
-		a[d[item]] = list_value
+
+	a[d[item]] = list_value
 
 print(a)
