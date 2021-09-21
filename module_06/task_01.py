c2558f16ee9e6826f15498be5a58d4bb75fcab8b
Nataliia Tenkova <natalia__korotkova@mail.ru>
2021-09-21 20:29:59 +0300

    Union list in task_01

--- module_06/task_01.py
+++ module_06/task_01.py
@@ -5,8 +5,7 @@ def get_thread(thread_name):
 	time.sleep(1)
 	print(f'start {thread_name}')
 
-threads_names = ['thread 1', 'thread 2', 'thread 3', 'thread 4', 'thread 5']
-threads = [Thread(target = get_thread, args=(n, )) for n in threads_names]
+threads = [Thread(target = get_thread, args=(n, )) for n in ['thread 1', 'thread 2', 'thread 3', 'thread 4', 'thread 5']]
 
 for t in threads:
 	t.start()
