0a65a6d96fefeef09a725be78ce22bcffb19896a
Nataliia Tenkova <natalia__korotkova@mail.ru>
2021-10-07 21:01:44 +0300

    change task_03

--- module_08/task_03.py
+++ module_08/task_03.py
@@ -7,38 +7,38 @@ class BaseModel(Model):
         database = conn
 
 class Students(BaseModel):
-    id_st = PrimaryKeyField()
-    name = CharField(column_name = 'name')
-    surname = CharField(column_name = 'surname')
-    age = IntegerField(column_name = 'age')
-    city = CharField(column_name = 'city')
+    id = PrimaryKeyField()
+    name = CharField()
+    surname = CharField()
+    age = IntegerField()
+    city = CharField()
 
 class Courses(BaseModel):
-    id_c = PrimaryKeyField()
-    name = CharField(column_name = 'name')
-    time_start = DateTimeField(column_name = 'time_start')
-    time_end = DateTimeField(column_name = 'time_end')
+    id = PrimaryKeyField()
+    name = CharField()
+    time_start = DateTimeField()
+    time_end = DateTimeField()
 
 class Student_Courses(BaseModel):
-	student_id = ForeignKeyField(Students, to_field = "id_st")
-	courses_id = ForeignKeyField(Courses, to_field = "id_c")
+	student_id = ForeignKeyField(Students, to_field = "id")
+	courses_id = ForeignKeyField(Courses, to_field = "id")
 
 # Students.create_table()
 # Courses.create_table()
 # Student_Courses.create_table()
 
-# student = Students(id_st = 1, name = "Max", surname = "Brooks", age=24 , city = "Spb")
+# student = Students(name = "Max", surname = "Brooks", age=24 , city = "Spb")
 # student.save()
-# student = Students(id_st = 2, name = "John", surname = "Stones", age=15 , city = "Spb")
+# student = Students(name = "John", surname = "Stones", age=15 , city = "Spb")
 # student.save()
-# student = Students(id_st = 3, name = "Andy", surname = "Wings", age=45 , city = "Manchester")
+# student = Students(name = "Andy", surname = "Wings", age=45 , city = "Manchester")
 # student.save()
-# student = Students(id_st = 4, name = "Kate", surname = "Brooks", age=34 , city = "Spb")
+# student = Students(name = "Kate", surname = "Brooks", age=34 , city = "Spb")
 # student.save()
 
-# courses = Courses(id_c = 1, name = "python", time_start = "21.07.2021", time_end = "21.08.2021")
+# courses = Courses(name = "python", time_start = "21.07.2021", time_end = "21.08.2021")
 # courses.save()
-# courses = Courses(id_c = 2, name = "java", time_start = "13.07.2021", time_end = "16.08.2021")
+# courses = Courses(name = "java", time_start = "13.07.2021", time_end = "16.08.2021")
 # courses.save()
 
 # st_c = Student_Courses(student_id = 1, courses_id = 1)
@@ -53,7 +53,17 @@ class Student_Courses(BaseModel):
 for student in Students.select().where(Students.age > 30):
 	print(student.name)
 
-for st_c in Student_Courses.select().join(Students).join(Courses):
- 	print(st_c.Students.id_st)
+print("_____________")
+
+query = Students.select().join(Student_Courses).join(Courses).where(Courses.name == 'python')
+for student in query:
+    print(student.name, student.city)
+
+print("_____________")
+
+query2 = Students.select().join(Student_Courses).join(Courses).where((Students.city == 'Spb') & (Courses.name == 'python'))
+for st in query2:
+    print(st.name)
+
 
 conn.close()
