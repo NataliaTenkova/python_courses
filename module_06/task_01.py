2549513606a186935138230bf90189082d9ef972
Nataliia Tenkova <natalia__korotkova@mail.ru>
2021-10-11 21:50:06 +0300

    update task_01
    добавлена возможность запуска с переменным кол-вом потоков

--- module_06/task_01.py
+++ module_06/task_01.py
@@ -5,7 +5,14 @@ def get_thread(thread_name):
 	time.sleep(1)
 	print(f'start {thread_name}')
 
-threads = [Thread(target = get_thread, args=(n, )) for n in ['thread 1', 'thread 2', 'thread 3', 'thread 4', 'thread 5']]
+def gen_threads(n):
+	list_thread = ["thread " + str(i + 1) for i in range(n)]
+	return list_thread
+
+n = 10
+list_thread = gen_threads(n)
+
+threads = [Thread(target = get_thread, args=(n, )) for n in list_thread]
 
 for t in threads:
 	t.start()
