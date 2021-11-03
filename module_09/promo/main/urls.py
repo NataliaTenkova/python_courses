e44ff4fbbae13ca274773f8e69fa8b8c35b7d8e2
Nataliia Tenkova <natalia__korotkova@mail.ru>
2021-11-03 11:03:29 +0300

    Delete module_09/promo directory

--- module_09/promo/main/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path, include
-from . import views
-
-urlpatterns = [
-    path('index', views.index, name = 'Index'),
-    path('reg', views.reg, name = 'Reg'),
-    path('user', views.user, name = 'User'),
-
-]
